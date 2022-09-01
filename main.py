656f8b8da23beb6f8335ae7408f2c665629529d9
Eddie <eddie.thomas@semanticarts.com>
2022-08-31 22:59:57 -0600

    Remove the `()` for the callback function

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src import Security
 
 if (__name__ == "__main__"):
     SECURITY_CLASS = Security()
-    SECURITY_CLASS.button.when_pressed = SECURITY_CLASS.toggleBacklight()
+    SECURITY_CLASS.button.when_pressed = SECURITY_CLASS.toggleBacklight
 
 
