7589778fe41d0b60ba64bda1cc2a1483766bde8d
Eddie <eddie.thomas@semanticarts.com>
2022-08-31 23:17:27 -0600

    Remove direct reference for toggling the backlight & add extra print statements

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from src import Security
 
 if (__name__ == "__main__"):
     SECURITY_CLASS = Security()
+    def whenPressed():
+        SECURITY_CLASS.toggleBacklight()
+
     SECURITY_CLASS.button.wait_for_press()
     print("pressed")
-    SECURITY_CLASS.button.when_pressed = SECURITY_CLASS.toggleBacklight
+    SECURITY_CLASS.button.when_pressed = whenPressed
+    SECURITY_CLASS.button.wait_for_released()
+    print("done")
 
 
