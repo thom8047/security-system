09f8561afae61d682d490805be4e5ae3e7471121
eddie-semarts <eddie.thomas@semanticarts.com>
2022-09-01 16:31:10 -0600

    Add back the disable docstring

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,8 @@
 
 from time import sleep
 
-# pylint: disable=import-error, missing-module-docstring
+# pylint: disable=import-error
+# pylint: disable=C0114
 from RPLCD.i2c import CharLCD as LCD
 import gpiozero as zero
 
