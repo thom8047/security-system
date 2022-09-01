6b935c0633bab478227722d86929d0347d36f9cf
Eddie <eddie.thomas@semanticarts.com>
2022-08-31 23:08:03 -0600

    Add some logs to see why things aren't being awaited

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src import Security
 
 if (__name__ == "__main__"):
     SECURITY_CLASS = Security()
+    SECURITY_CLASS.button.wait_for_press()
+    print("pressed")
     SECURITY_CLASS.button.when_pressed = SECURITY_CLASS.toggleBacklight
 
 
