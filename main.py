9bb76942d9a8f6a5dd985dbf598ce4eff01e4d2a
eddie-semarts <eddie.thomas@semanticarts.com>
2022-09-02 16:48:17 -0600

    Add async gathering behavior for coroutine execution

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from src import Security
 
 async def main():
     """Main execution"""
-    security = {}
-    security.operation = Security()
+    security = Security()
 
     # Run various security methods asynchronously
-    asyncio.run(security.operation.handle_toggle_button_interaction())
+    asyncio.gather(security.handle_toggle_button_interaction())
 
 
 if __name__ == "__main__":
