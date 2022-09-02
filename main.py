cda12a5d2bb8f685fc87c8a6e96e37d953e2923a
eddie-semarts <eddie.thomas@semanticarts.com>
2022-09-02 17:07:32 -0600

    Try again switch the gathering methods order

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ async def main():
         Args:
             i (int): number
         """
-        await asyncio.sleep(5)
-        security.write_output_to_lcd(f"Awaited 5 seconds {i}")
+        await asyncio.sleep(1)
+        security.write_output_to_lcd(f"waited {i} seconds")
         await multiple_writing(i + 1)
 
     # Run various security methods asynchronously
     await asyncio.gather(
-        security.handle_toggle_button_interaction(),
         multiple_writing(0),
+        security.handle_toggle_button_interaction(),
     )
 
 
