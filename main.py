256ab671167b7648ffacf4fa36060865fb79bef9
eddie-semarts <eddie.thomas@semanticarts.com>
2022-09-02 16:59:52 -0600

    Add second async function for testing

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ async def main():
     """Main execution"""
     security = Security()
 
+    async def multiple_writing(i):
+        """testing method
+        Args:
+            i (int): number
+        """
+        await asyncio.sleep(5)
+        security.write_output_to_lcd(f"Awaited 5 seconds {i}")
+        await multiple_writing(i + 1)
+
     # Run various security methods asynchronously
-    await asyncio.gather(security.handle_toggle_button_interaction())
+    await asyncio.gather(
+        security.handle_toggle_button_interaction(),
+        multiple_writing(0),
+    )
 
 
 if __name__ == "__main__":
