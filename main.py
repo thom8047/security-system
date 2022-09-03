1fd08a25a0422547356bb1afb55085b897e6817e
eddie-semarts <eddie.thomas@semanticarts.com>
2022-09-02 18:17:31 -0600

    Address pylint failure

--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import asyncio
 import datetime
 from src import Security
 
+# Top-level security singleton
+security = Security()
+
+
+async def multiple_writing(i):
+    """testing method
+    Args:
+        i (int): number
+    """
+
+    await asyncio.sleep(30)
+    [hour, minute, suffix] = [
+        datetime.datetime.now().hour,
+        datetime.datetime.now().minute,
+        "PM" if datetime.datetime.now().hour > 11 else "AM",
+    ]
+    security.clear_lcd_display()
+    security.write_output_to_lcd(
+        f"Date: {datetime.date.today()}Time: {12 if hour == 0 else hour - 12 if hour > 12 else hour}:{minute:02} {suffix}"
+    )
+    await multiple_writing(i + 1)
+
 
 async def main():
     """Main execution"""
-    security = Security()
-
-    async def multiple_writing(i):
-        """testing method
-        Args:
-            i (int): number
-        """
-        await asyncio.sleep(30)
-        [hour, minute, suffix] = [
-            datetime.datetime.now().hour,
-            "{:02d}".format(datetime.datetime.now().minute),
-            "PM" if datetime.datetime.now().hour > 11 else "AM",
-        ]
-        security.clear_lcd_display()
-        security.write_output_to_lcd(
-            f"Date: {datetime.date.today()}Time: {12 if hour == 0 else hour - 12 if hour > 12 else hour}:{minute} {suffix}"
-        )
-        await multiple_writing(i + 1)
 
     # Run various security methods asynchronously
     await asyncio.gather(
